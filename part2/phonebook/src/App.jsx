bf1e2902cbdf765432b8a6c79b020fa4261f4a92
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-01-28 21:58:27 +0800

    added db.json, extract API calls to its own module

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ handleFilter }) => {
   return (
@@ -11,7 +12,6 @@ const Filter = ({ handleFilter }) => {
 const PersonForm = ({ addContact, handleNameChange, newName, handleNumberChange, newNumber }) => {
   return (
     <form onSubmit={addContact}>
-        <h2>add a new</h2>
         <div>
           name: <input onChange={handleNameChange} value={newName}/>
         </div>
@@ -44,15 +44,24 @@ const Person = ({ person }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [personsToShow, setPersonsToShow] = useState(persons)
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log(response)
+        setPersons(response.data)
+        setPersonsToShow(response.data)
+      })
+
+    console.log("effect")
+  }, [])
+
+  console.log("render")
 
   const addContact = (event) => {
     event.preventDefault()
