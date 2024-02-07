ba2710631a75e914d94c3acea017df29dcc10270
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-02-07 17:18:08 +0800

    Saving data to MongoDB: Fetching objects from the database

---
+++ notesBackend/mongoose.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@clusternotes.dknferp.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'Getting a job is hard',
+  important: true,
+})
+
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  console.log(result)
+  mongoose.connection.close()
+})
+*/
+
+Note.find({ content: 'Getting a job is hard' }).then(result => {
+    console.log(result)
+    mongoose.connection.close()
+})
