35f89c7499afe8a4e49f222f35a834c8c25e1257
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-02-10 22:04:18 +0800

    4.4: Helper Functions and Unit Tests, step 2

---
+++ part4/blogListApp/utils/list_helper.js
@@ -0,0 +1,19 @@
+const dummy = (blogs) => {
+  console.log(blogs)
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  let totalLikes = 0
+
+  blogs.forEach(blog => {
+    totalLikes += blog.likes
+  })
+
+  return totalLikes
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
