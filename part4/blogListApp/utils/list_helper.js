b035737669b57a1d4bafbea9272f9e9e8c3d321d
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-02-11 00:38:32 +0800

    4.6*: Helper Functions and Unit Tests, step 4

--- part4/blogListApp/utils/list_helper.js
+++ part4/blogListApp/utils/list_helper.js
@@ -13,7 +13,77 @@ const totalLikes = (blogs) => {
   return totalLikes
 }
 
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined
+  }
+  let mostPopularBlog = blogs[0]
+
+  blogs.forEach(blog => {
+    mostPopularBlog = blog.likes > mostPopularBlog.likes ? blog : mostPopularBlog
+  })
+
+  mostPopularBlog = {
+    title: mostPopularBlog.title,
+    author: mostPopularBlog.author,
+    likes: mostPopularBlog.likes
+  }
+
+  return mostPopularBlog
+}
+
+const createAuthorsArray = (blogs) => {
+  const authors = []
+
+  blogs.forEach(blog => {
+    const name = blog.author
+    let isAuthorInArray = false
+
+    for (let i = 0; i < authors.length; i++) {
+      if (authors[i].author === name) {
+        isAuthorInArray = true
+        break
+      }
+    }
+    if (isAuthorInArray === false) {
+      authors.push({
+        author: name,
+        blogs: 0
+      })
+    }
+  })
+
+  console.log(authors)
+  return authors
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return undefined
+  }
+
+  const authors = createAuthorsArray(blogs)
+  let maxBlogs = 0
+
+  authors.forEach(author => {
+    blogs.forEach(blog => {
+      if (blog.author === author.author) {
+        author.blogs += 1
+      }
+    })
+    maxBlogs = author.blogs > maxBlogs ? author.blogs : maxBlogs
+  })
+
+  console.log(authors)
+  console.log(maxBlogs)
+  console.log(authors.find(author => author.blogs === maxBlogs))
+  return authors.find(author => author.blogs === maxBlogs)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog,
+  createAuthorsArray,
+  mostBlogs
 }
