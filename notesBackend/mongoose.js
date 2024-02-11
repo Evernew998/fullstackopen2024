dc8cefedbc38f00a81e974874510a23eb62330d8
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-02-11 22:05:47 +0800

    Running tests one by one

--- notesBackend/mongoose.js
+++ notesBackend/mongoose.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@clusternotes.dknferp.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@clusternotes.dknferp.mongodb.net/testNoteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -20,7 +20,7 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'Getting a job is hard',
+  content: 'This is note 2',
   important: true,
 })
 
