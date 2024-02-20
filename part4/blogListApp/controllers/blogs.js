76d6209989470fff07776b5799b8c73e521af32a
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-02-20 18:58:42 +0800

    4.21*: Blog List Expansion, step 9

--- part4/blogListApp/controllers/blogs.js
+++ part4/blogListApp/controllers/blogs.js
@@ -54,9 +54,42 @@ blogRouter.post('/', async (request, response, next) => {
 })
 
 blogRouter.delete('/:id', async (request, response, next) => {
-  const id =  request.params.id
+  const blogId =  request.params.id
+
   try {
-    await Blog.findByIdAndDelete(id)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!decodedToken.id) {
+      return response.status(401).json({
+        error: 'invalid token'
+      })
+    }
+
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(blogId)
+
+    if (!user) {
+      return response.status(400).json({
+        error: 'invalid user'
+      })
+    }
+
+    if (!blog) {
+      return response.status(400).json({
+        error: 'invalid blog id'
+      })
+    }
+
+    if (blog.user.toString() !== user._id.toString()) {
+      return response.status(401).json({
+        error: 'user is unauthorized to delete blog'
+      })
+    }
+
+    await Blog.findByIdAndDelete(blog._id)
+    user.blogs = user.blogs.filter(b => b.toString() !== blog._id.toString())
+
+    await user.save()
     response.status(204).end()
   }
   catch (exception) {
