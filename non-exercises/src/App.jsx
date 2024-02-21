e042f5ac7fbc2d968d0a854405b8f995df87a4c1
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-02-21 18:02:48 +0800

    Handling login

--- non-exercises/src/App.jsx
+++ non-exercises/src/App.jsx
@@ -3,12 +3,18 @@ import Note from './components/Note'
 import Notification from './components/Notifications'
 import Footer from './components/Footer'
 import noteService from './services/notes'
+import loginService from './services/login'
+import LoginForm from './components/LoginForm'
+import NoteForm from './components/NoteForm'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     noteService
@@ -39,7 +45,7 @@ const App = () => {
   
     noteService
       .update(id, changedNote)
-      .then(returnedNote => {
+        .then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(() => {
@@ -56,6 +62,32 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePaswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important)
@@ -64,6 +96,24 @@ const App = () => {
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+
+      {!user && <LoginForm 
+          handleLogin={handleLogin}
+          username={username}
+          password={password}
+          handleUsernameChange={handleUsernameChange}
+          handlePasswordChange={handlePaswordChange}
+        />
+      }
+      {user && <div>
+        {/*<p>{user.name} logged in</p>*/}
+          <NoteForm 
+            addNote={addNote}
+            newNote={newNote}
+            handleNoteChange={handleNoteChange}
+          />
+        </div>
+      } 
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
@@ -78,13 +128,6 @@ const App = () => {
           />
         )}
       </ul>
-      <form onSubmit={addNote}>
-      <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>
       <Footer />
     </div>
   )
