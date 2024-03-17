d3191ea7c46c31cceefe9058291e48157f179ef9
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-03-17 16:35:07 +0800

    Test Coverage

--- non-exercises/src/components/NoteForm.jsx
+++ non-exercises/src/components/NoteForm.jsx
@@ -3,6 +3,10 @@ import { useState } from 'react'
 const NoteForm = ({ createNote }) => {
   const [newNote, setNewNote] = useState('')
 
+  const handleChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
   const addNote = (event) => {
     event.preventDefault()
     createNote({
@@ -14,15 +18,20 @@ const NoteForm = ({ createNote }) => {
   }
 
   return (
-    <div>
+    <div className="formDiv">
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
         <input
           value={newNote}
-          onChange={(event) => setNewNote(event.target.value)}
+          onChange={handleChange}
+          placeholder='write note content here'
         />
-        <button type='submit'>save</button>
+        <input
+          value="..."
+          onChange="..."
+        />
+        <button type="submit">save</button>
       </form>
     </div>
   )
