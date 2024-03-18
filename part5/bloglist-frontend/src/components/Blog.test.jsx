5c3e2701869527ed401be0e72a76babe3db24173
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-03-18 13:01:40 +0800

    5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { expect } from 'vitest'
 
-test('renders blog title and author but not blog url', () => {
+describe('<Blog />', () => {
   const blog = {
     title: 'blog title',
     author: 'blog author',
@@ -21,14 +21,32 @@ test('renders blog title and author but not blog url', () => {
     name: 'laici'
   }
 
-  const { container } = render(<Blog blog={blog} user={user} />)
+  let container
 
-  const blogTitleAndAuthor = screen.getByText('blog title blog author')
-  expect(blogTitleAndAuthor).toBeDefined()
+  beforeEach(() => {
+    container = render(<Blog blog={blog} user={user} />).container
+  })
 
-  const blogInfo = container.querySelector('.blogInfo')
-  expect(blogInfo).not.toHaveStyle('display: none')
+  test('renders blog title and author but not blog url or blog likes', () => {
 
-  const blogInfoFull = container.querySelector('.blogInfoFull')
-  expect(blogInfoFull).toHaveStyle('display: none')
+    const blogTitleAndAuthor = screen.getByText('blog title blog author')
+    expect(blogTitleAndAuthor).toBeDefined()
+
+    const blogInfo = container.querySelector('.blogInfo')
+    expect(blogInfo).not.toHaveStyle('display: none')
+
+    const blogInfoFull = container.querySelector('.blogInfoFull')
+    expect(blogInfoFull).toHaveStyle('display: none')
+  })
+
+  test('clicking the view button displays the blog URL and number of likes', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    screen.debug()
+
+    const blogInfoFull = container.querySelector('.blogInfoFull')
+    expect(blogInfoFull).not.toHaveStyle('display: none')
+  })
 })
