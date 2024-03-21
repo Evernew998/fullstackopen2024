18459739b1745313452eefc8a23cee15e11a85c7
Evernew998 <136622872+Evernew998@users.noreply.github.com>
2024-03-21 15:09:56 +0800

    5.18: Blog List End To End Testing, step2

--- part5/blogs-e2e/tests/blog_app.spec.js
+++ part5/blogs-e2e/tests/blog_app.spec.js
@@ -25,4 +25,27 @@ describe('Blog app', () => {
     
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('salainen')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByTestId('username').fill('mluukkai')
+      await page.getByTestId('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      const errorDiv = page.locator('.error')
+      await expect(errorDiv).toHaveCSS('border-style', 'solid')
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+      await expect(errorDiv).toContainText('wrong username or password')
+
+      await expect(page.getByText('Matti Luukkainen logged in')).not.toBeVisible()
+    })
+  })
 })
